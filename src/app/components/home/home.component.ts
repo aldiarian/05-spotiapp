2c920860b3e2ede59cd466da5c445c58024fc58f
Alberto Diarian Sancho <aldiarian@gmail.com>
2019-03-26 19:14:14 +0100

    fin de apllicacion

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -9,15 +9,22 @@ import { SpotifyService } from 'src/app/services/spotify.service';
 export class HomeComponent  {
 
   loading:boolean;
+  error:boolean;
+  mensajeError: string;
   nuevasCanciones:any[] = [];
   
   constructor( private spotify: SpotifyService) {
     this.loading = true;
+    this.error = false;
     this.spotify.getNewReleases()
       .subscribe( data => {
         this.nuevasCanciones = data;
         this.loading = false;
-      });
+      }, ( errorSuscripcion => {
+        this.loading = false;
+        this.error = true;
+        this.mensajeError = errorSuscripcion.error.error.message;
+      }  ));
     }
 
 
